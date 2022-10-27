8de0cee4b9c773d628631f80e77f7d1c8cb2396c
Jason Tran <jasonhoang.tran@gmail.com>
2022-10-27 16:45:23 -0400

    3rd commit with admin endpoints

--- CapstoneProject/src/main/java/com/learning/controller/ApproverController.java
+++ CapstoneProject/src/main/java/com/learning/controller/ApproverController.java
@@ -1,5 +1,27 @@
 package com.learning.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
 public class ApproverController {
 
+	@PostMapping("/staff")
+	public void createStaff() {
+		
+	}
+	
+	@GetMapping("staff")
+	public void getAllStaff() {
+		
+	}
+	
+	@PutMapping("/staff")
+	public void enableStaff() {
+		
+	}
 }
