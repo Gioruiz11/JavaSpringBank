8395d918ff18fd10cd47d81762e295dc30ea65fa
jeansapini <jeansapini@localhost>
2022-10-28 16:26:25 -0400

    Customer

--- CapstoneProject/src/main/java/com/learning/entity/Customer.java
+++ CapstoneProject/src/main/java/com/learning/entity/Customer.java
@@ -1,8 +1,22 @@
 package com.learning.entity;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class Customer {
+	@Id
+	private long customerId;
+	private String fullName;
+	private String userName;
+	private String password;
+	private String accountType;
+	private double accountBalance;
+	private String approved;
+	private long accountNumber;
+	private LocalDateTime dateOfCreation;
+	private String accountStatus;
 	
 }
