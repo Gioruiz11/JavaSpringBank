dd3cea89b8562b7c3bef608b56e9998f86ece04a
owner <owner@DESKTOP-6PFLC6L>
2022-10-30 14:30:26 -0400

    Staff entity no getters or setters

--- CapstoneProject/src/main/java/com/learning/entity/Staff.java
+++ CapstoneProject/src/main/java/com/learning/entity/Staff.java
@@ -1,8 +1,21 @@
 package com.learning.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import org.springframework.beans.factory.annotation.Value;
 
 @Entity
 public class Staff {
+	@Id
+	private Long staffId;
+	private String staffFullName;
+	private String staffUserName;
+	private String staffPassword;
+    private enum Status {
+		enable, disable
+    }
 	
+	
+
 }
