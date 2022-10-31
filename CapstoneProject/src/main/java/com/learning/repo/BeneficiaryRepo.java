869c1073759d4b0608bab61395bde8cb020cbcac
owner <owner@DESKTOP-6PFLC6L>
2022-10-31 16:48:25 -0400

    BeneficiaryRepo

---
+++ CapstoneProject/src/main/java/com/learning/repo/BeneficiaryRepo.java
@@ -0,0 +1,9 @@
+package com.learning.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.learning.entity.Staff;
+
+//public interface BeneficiaryRepo extends JpaRepository<Beneficiary, Long> {
+
+//}
