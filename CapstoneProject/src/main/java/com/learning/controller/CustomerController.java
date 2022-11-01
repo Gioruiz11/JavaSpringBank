7599ae9532df19e94d8b1518ab565cebd90a1da3
jeansapini <jeansapini@localhost>
2022-11-01 17:32:23 -0400

    AccountRepo Added CustomerController Updated

--- CapstoneProject/src/main/java/com/learning/controller/CustomerController.java
+++ CapstoneProject/src/main/java/com/learning/controller/CustomerController.java
@@ -1,32 +1,48 @@
 package com.learning.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.learning.entity.Account;
+import com.learning.entity.Customer;
+import com.learning.repo.AccountRepo;
+import com.learning.repo.CustomerRepo;
+
 @Controller
+@CrossOrigin
 @RequestMapping("/api/customer")
 public class CustomerController {
-	//@PostMapping("/register")
-	public void registerCustomer() {
-		
+	@Autowired
+	CustomerRepo customerRepo;
+	@Autowired
+	AccountRepo accountRepo;
+	
+	@PostMapping("/register")
+	public Customer registerCustomer(@RequestBody Customer customer) {
+		return customerRepo.save(customer);
 	}
-	//@PostMapping("/{customerId}/account")
-	public void createCustomerAccount() {
-		
+	@PostMapping("/{customerId}/account")
+	public Account createAccount(@RequestBody Account account) {
+		return accountRepo.save(account);
 	}	
-	//@PutMapping("/{customerId}/account/{accountNumber}")
+	@PutMapping("/{customerId}/account/{accountNumber}")
 		
-	//@GetMapping("/{customerId}/account")
-	public void getAccountByCustomerId() {
-		
+	@GetMapping("/{customerId}/account")
+	public List<Account> getAccountByCustomerId(long customerId) {
+		return accountRepo.findAll();
 	}
-	//@GetMapping("/{customerId}")
-	public void getCustomerByCustomerId() {
-		
+	@GetMapping("/{customerId}")
+	public List<Customer> getCustomerByCustomerId(long customerId) {
+		return customerRepo.findAll();
 	}
 	//Jean's Methods
 	
