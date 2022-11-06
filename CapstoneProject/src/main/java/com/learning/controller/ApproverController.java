a7bcde24502af2bb8387bfa26380beaa4c7fe180
Robert Joasilus <RobertJoasilus@gmail.com>
2022-11-06 11:58:04 -0500

    Controller Fix

--- CapstoneProject/src/main/java/com/learning/controller/ApproverController.java
+++ CapstoneProject/src/main/java/com/learning/controller/ApproverController.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityExistsException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.learning.repo.StaffRepo;
 import com.learning.service.ApproverService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/admin")
 public class ApproverController {
 	@Autowired
--- CapstoneProject/src/main/java/com/learning/controller/StaffController.java
+++ CapstoneProject/src/main/java/com/learning/controller/StaffController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,6 +22,7 @@ import com.learning.entity.Transaction;
 import com.learning.service.StaffService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/staff")
 public class StaffController {
 
