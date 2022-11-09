b4ec55095e17bde77291986d73222a2b96a73472
skawuma <s_kawuma@salemstate.edu>
2022-11-09 09:04:34 -0500

    corrected errors in approve service and controller

--- CapstoneProject/src/main/java/com/learning/service/ApproverService.java
+++ CapstoneProject/src/main/java/com/learning/service/ApproverService.java
@@ -11,21 +11,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.learning.entity.Staff;
 import com.learning.repo.CustomerRepo;
 import com.learning.repo.StaffRepo;
+
 @Service
 public class ApproverService {
-
 
 	@Autowired
 	CustomerRepo customerRepo;
 	@Autowired
 	StaffRepo staffRepo;
 
-	public Staff createStaff(@RequestBody Staff staff){
+	public Staff createStaff(@RequestBody Staff staff) {
 		Staff s = new Staff();
 		s.setStaffId(staff.getStaffId());
-		s.setStaffFullName(staff.getStaffFullName() );
+		s.setStaffFullName(staff.getStaffFullName());
 		s.setStaffUserName(staff.getStaffUserName());
-		s.setStatus(staff.isStatus());	 		 
+		s.setStatus(staff.isStatus());
 		return staffRepo.save(staff);
 
 	}
@@ -35,10 +35,13 @@ public class ApproverService {
 
 	}
 
-	
-	
-	
-	
+	public Staff getstaffByid(Long staffId) {
+		return staffRepo.findById(staffId).get();
 	}
-	
 
+	public void saveStaff(Staff staff) {
+
+		staffRepo.save(staff);
+	}
+
+}
